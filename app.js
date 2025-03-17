8f2e77e776638d89fb1406d5cba56386ba6f5fd3
libantoB <libanto@hotmail.com>
2025-03-17 17:46:47 -0500

    se crea una función que genera un amigo aleatorio y esto permite saber cual es el amigo secreto, ademas se realizo la verificación que el array no este vacio.

--- app.js
+++ app.js
@@ -10,9 +10,7 @@ function agregarAmigo(){
         alert("Escribe un nombre")
     } else {
         amigos.push(amigo);
-
         input.value= "";
-
         console.log(amigos)
         crearLista();
     }
@@ -26,4 +24,17 @@ function crearLista(){
         listaAmigos.appendChild(li); 
     }
 }
+//Sorteo para el amigo secreto
+function sortearAmigo(){
+    let resultado = document.getElementById('resultado');
 
+    if (amigos.length === 0) {
+        resultado.innerHTML = "No hay amigos en la lista.";
+        return;
+    }
+
+    let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    let amigoSorteado = amigos[indiceAleatorio];
+
+    resultado.innerHTML = `El amigo sorteado es: ${amigoSorteado}`;
+}
