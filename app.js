e7df55b053561cac6f75004aa6c77a33cd9e8cef
libantoB <libanto@hotmail.com>
2025-03-17 16:35:04 -0500

    Creación guardar lista de amigos en el array, limpieza del input

--- app.js
+++ app.js
@@ -2,5 +2,19 @@
 let amigos = [];
 
 function agregarAmigo(){
+    let input = document.getElementById('amigo');
+    let amigo = input.value.trim();
+
+    if(amigo === ''){
+        alert("Escribe un nombre")
+    } else {
+        amigos.push(amigo);
+
+        input.value= "";
+
+        console.log(amigos)
+    }
+    
     
 }
+
